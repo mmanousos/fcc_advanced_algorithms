386608c924ff5b4ca8551fd5d1489cc2398922ae
M. Manousos <mmanousos@gmail.com>
2017-12-23 13:13:03 -0800

    pseudocode

--- validatephone.js
+++ validatephone.js
@@ -1,4 +1,12 @@
+/*-- PSEUDOCODE --*/
 
+// receive a string
+// get \d & check length: if > 10 or < 9, return false
+// check if contains characters other than -(), spaces or numbers
+    // if so, return false
+// if 10 digits and first digit is other than 1, return false
+
+// also need to check that - and () are in correct places
 
 /*-- ASSIGNMENT --*/
 
@@ -35,10 +43,10 @@ telephoneCheck("1 555 555 5555") should return true.
 telephoneCheck("1 456 789 4444") should return true.
 telephoneCheck("123**&!!asdf#") should return false.
 telephoneCheck("55555555") should return false.
-telephoneCheck("(6505552368)") should return false
+telephoneCheck("(6505552368)") should return false.
 telephoneCheck("2 (757) 622-7382") should return false.
 telephoneCheck("0 (757) 622-7382") should return false.
-telephoneCheck("-1 (757) 622-7382") should return false
+telephoneCheck("-1 (757) 622-7382") should return false.
 telephoneCheck("2 757 622-7382") should return false.
 telephoneCheck("10 (757) 622-7382") should return false.
 telephoneCheck("27576227382") should return false.

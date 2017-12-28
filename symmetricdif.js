a706fe110c94e22ff1e6b588b1f5e9297eab9305
M. Manousos <mmanousos@gmail.com>
2017-12-27 16:09:13 -0800

    a solution (not passing fcc checks)

--- symmetricdif.js
+++ symmetricdif.js
@@ -1,15 +1,262 @@
 
+// convert object to arrays
+// concat first two arrays
+// filter for duplicates
+// set filtered array as new first array & get next array
+// repeat
+// sort ascending 
+// return
+
+
+
+
+
+
+// convert object to arrays
+// get each element of first array
+// check if it is present in the next array
+// if no, add to new array 
+
+// get each element of second array
+// check if it is present in the first array
+// if no, add to new array 
+  
+    // set new array as first array & move to 3rd array, run again cycling through until no more arrays remain
+// sort array ascending
+// return new array
+
+/* works but not not passing fcc checks */ 
+
+function sym(args) {
+  var returnArr = []; 
+  args = Array.prototype.slice.call(arguments);
+  var count = args.length;
+  
+  var arr1 = arguments[0];
+  var arr2 = arguments[1];
+  
+  function diff() {
+  
+    for (var i = 0; i < arr1.length; i++) {
+      var val1 = arr1[i];
+
+      if (arr2.indexOf(val1) < 0) {
+        if (returnArr.indexOf(val1) < 0) {
+        returnArr.push(val1);
+        }
+      }
+    }
+    
+    for (var j = 0; j < arr2.length; j++) {
+      var val2 = arr2[j];
+      if (arr1.indexOf(val2) < 0) {
+        if (returnArr.indexOf(val1) < 0) {
+        returnArr.push(val2);
+       }
+      }
+    }
+  }
+ diff();
+ 
+ if (count > 2) {
+   var holdArr = returnArr;
+   var arr1 = arguments[2];
+   for (var k = 0; k < holdArr.length; k++) {
+      var val1 = arr1[k];
+      if (arr1.indexOf(val1) < 0) {
+        if (returnArr.indexOf(val1) < 0) {
+        returnArr.push(val1);
+      }
+    }
+   }
+ }
+ 
+ function ascending ( a, b ) {
+  return a-b;
+ }
+
+
+ var sorted = returnArr.sort( ascending );
+ return sorted;
+  
+}
+
+sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3], [5, 3, 9, 8], [1]);
+
+
+
+
+
+
+function sym(args) {
+ var returnArr = [];
+ var args = Array.prototype.slice.call(arguments);
+ var count = args.length;
+ 
+ /* filter()
+var arr = [1,2,3,4];
+var arrTransformed = arr.filter(function(element/val, index, arr) {
+  return index % 2; 
+}); 
+console.log(arrTransformed); // [2, 4]
+*/
+/*
+var arrTrans = args[1].filter(function(el) {
+  var position = args[1].indexOf(el);  
+});
+ */
+ 
+ var arr1 = args[0];
+ var arr2 = args[1];
+ for (var i = 0; arr1.length; i++) {
+   var index1 = arr1[i];
+   if (arr2.indexOf(index1) < 0) {
+     returnArr.push(index1);
+   }
+ }
+  
+  for (var i = 0; i < arr2.length; i++) {
+    var index2 = arr2[i]; 
+    if (arr2.indexOf(index2) == -1) {
+      returnArr.push(index2); 
+    }  
+  }
+
+  for (var j = 0; j < arr2.length; j++) {
+    var index2 = arr2[j]; 
+    if (arr1.indexOf(index2) == -1) {
+      newArr.push(index2); 
+    }  
+  }
+
+  return returnArr;
+}
+
+sym([1, 2, 3], [5, 2, 1, 4]);
+ 
+ 
+ /*
+ function check() {
+  for (var i = 0; i < args.length; i++) {
+   args[i].forEach(function(element) {
+     //cycles through all values of the argument arrays
+     
+
+    var present = (args.indexOf(element, 0));
+    console.log(element + " is present at location " + present);
+    if (present < 0) {
+      returnArr.push(element);
+      // args[i].splice(i,1);
+      // args[1].splice(present,1);
+    }
+   });
+  }
+ }
+
+ check();
+ 
+ return returnArr;
+*/
+
+
+
+
+
+
+/*
+function sym(args) {
+  var returnArr = [];
+  var argumentsLen = arguments.length;
+  var next = arguments[1];
+  for (var i = 0; i < argumentsLen; i++) {  
+  
+   function check(args) {
+   
+    args.forEach(function(element) {
+     var present = next.indexOf(element, 0);
+     if (present < 0) {
+       returnArr.push(element);
+     }  
+    });
+   } check(args);
+  }
+  
+  
+//  args.every(check, el);
+  
+//  return args;
+}*/
+
+
+
+
+/* forEach()
+e.g. const arr = ['a', 'b', 'c'];
+arr.forEach(function(element) {
+    console.log(element);
+});
+*/
+
+
+//arr.every(callback[, thisArg])
+
+/* every()
+function isBelowThreshold(currentValue) {
+  return currentValue < 40;
+}
+
+var array1 = [1, 30, 39, 29, 10, 13];
+
+console.log(array1.every(isBelowThreshold));
+// expected output: true
+*/
+
+/* filter()
+var arr = [1,2,3,4];
+var arrTransformed = arr.filter(function(element/val, index, arr) {
+  return index % 2; 
+}); 
+console.log(arrTransformed); // [2, 4]
+*/
+
+
+
+
+
+function sym(args) {
+ // convert object to array of arrays
+ var args = Array.prototype.slice.call(arguments);
+
+ // get total number of arrays    
+ var count = args.length;    
+    
+}
+
+sym([1, 2, 3], [5, 2, 1, 4]);
+
+
 /*-- PSEUDOCODE --*/
+
 
 // reduce all arrays to single array
 // remove all repaeated elements
-    // doesn't work because need to know if the element is in every array to count. 
+    /* doesn't work because need to know if the element is in every array to count. */
 
 // create empty array to return with result
 // get first element of first array
 // check if it 1) doesn't already exist in the new array, and 2) exists in every subsequent array
     // if yes, move to next element
     // if no, push to new array
+/* doesn't work very well because it's hard to check first array against subsequent arrays */
+
+
+// convert object to array of arrays
+// get total number of arrays
+// check each array for internal duplicates & remove (filter?)
+// combine first two arrays
+// check for duplicates and remove 
+  // repeat until no more arrays remain
+// return filtered array
 
 
 /*-- ASSIGNMENT --*/
@@ -35,3 +282,6 @@ sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3]) should return [2, 3,
 sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3]) should contain only five elements.
 sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3], [5, 3, 9, 8], [1]) should return [1, 2, 4, 5, 6, 7, 8, 9].
 sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3], [5, 3, 9, 8], [1]) should contain only eight elements.
+
+
+

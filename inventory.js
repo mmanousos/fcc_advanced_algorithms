4e770f2113012aa03e550b98c291da6f7856c629
M. Manousos <mmanousos@gmail.com>
2017-12-30 14:07:12 -0800

    refines script checking if items exist in inventory array

--- inventory.js
+++ inventory.js
@@ -1,3 +1,53 @@
+
+/* slight refinement to script checking if items exist */ 
+function updateInventory(arr1, arr2) {
+    // All inventory must be accounted for or you're fired!
+   
+
+ arr2.forEach(function(el, index, array) {
+  var list = arr1;
+  var item = el[1];
+  // checks if the item already exists & updates the quantity
+  for (var i = 0; i < array.length; i ++) {
+   var loc = list[i].indexOf(item);
+   if (loc > 0) {
+     arr1[i][0] = arr1[i][0] + el[0]; 
+     // removes item from new inventory list
+     arr2.splice(index, 1);
+   }  
+  } 
+});
+// add all new items to exisiting inventory
+ var inventory = arr1.concat(arr2);
+ 
+ // alphabetize, then
+ var invItems = [];
+ inventory.forEach(function(el) {
+  invItems.push(el[1]);
+});
+
+// sort the items in newInv alphabetically
+ invItems.sort();
+  
+ var updatedInventory = [];  
+  invItems.forEach(function(element, index) {
+   for (var m = 0; m < inventory.length; m++) {
+    var where = inventory[m].indexOf(element); 
+    if (where > 0) {
+     updatedInventory.push(inventory[m]);
+    }
+   }
+ });
+
+console.log(updatedInventory);
+ return updatedInventory;
+
+}
+
+updateInventory([[0, "Bowling Ball"], [0, "Dirty Sock"], [0, "Hair Pin"], [0, "Microphone"]], [[1, "Hair Pin"], [1, "Half-Eaten Apple"], [1, "Bowling Ball"], [1, "Toothpaste"]]);
+
+
+
 /* solution - does not pass all tests */ 
 /* fails for checking items in empty array -> check if either array is empty first!*/ 
 

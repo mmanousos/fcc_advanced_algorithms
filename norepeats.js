6d517b4ce341b0ace18f8057e7a0719b306633bb
M. Manousos <mmanousos@gmail.com>
2018-01-02 15:44:51 -0800

    creates permutations

--- norepeats.js
+++ norepeats.js
@@ -1,7 +1,7 @@
+/* uses Heap's algorithm to generate all permutations */ 
+// may make singleTest redundant
 
-/* successfully checks for single characters & for all repeats */ 
-
-
+/* next step is to create check for non-duplicates and count those */ 
 
 function permAlone(str) {
   var single = /^.{1}$/;
@@ -11,25 +11,105 @@ function permAlone(str) {
     return 1;
   }
   
-  function repeatCheck() { 
+//checks if string is all repeats 
+  function allRepeats() { 
    for (var i = 1; i < str.length; i++) {
     if (str.charAt(i) === first) {
       return 0;
     }
    }
   }
-  repeatCheck(); 
+  allRepeats(); 
   
+//splits string into array separated by character
+ var open = str.split('');    
+   
+// helper function to swap positions of items in an array    
+ function swap(array, posA, posB) {
+  var temp = array[posA]; 
+  array[posA] = array[posB]; 
+  array[posB] = temp; 
+ }
+
+// Heap's algorithm     
+  function permute(array, n) {
+    n = n || array.length; 
+    if (n === 1) {
+      return 1;
+    } else {
+      for (var i = 0; i < n-1; i++) {
+        permute(array, n-1);
+        if (n % 2 === 0) {
+          var j = 1;     
+        } else {
+          var j = i; 
+        }
+      } swap(array, j-1, n-1);
+    } 
+  }
+permute(open);
 }
 
 permAlone('aaa');
 
 
 
+
+
+
+/* successfully checks for single characters & for all repeats */ 
+
+function permAlone(str) {
+  var single = /^.{1}$/;
+  var singleTest = single.test(str); 
+  var first = str.charAt(0);
+  if (singleTest === true) {
+    return 1;
+  }
+  
+//checks if string is all repeats 
+  function allRepeats() { 
+   for (var i = 1; i < str.length; i++) {
+    if (str.charAt(i) === first) {
+      return 0;
+    }
+   }
+  }
+  allRepeats(); 
+  
+}
+
+permAlone('aaa');
+
+
 /*-- PSEUDOCODE --*/ 
 
 If agument only contains repeated letters, return 0. 
 If argument contains only 1 letter, return 1. 
+
+If neither of those end the function, begin creating permutations of the String
+Create an empty counter variable.  
+For each variation, check if the permutation has repeated characters
+    if it does not, increase the counter by 1. 
+after permutations have completed, return counter. 
+
+
+// pseudocode for Heap's algorithm for generating permutations
+
+procedure generate(n : integer, A : array of any):
+    if n = 1 then
+          output(A)
+    else
+        for i := 0; i < n - 1; i += 1 do
+            generate(n - 1, A)
+            if n is even then
+                swap(A[i], A[n-1])
+            else
+                swap(A[0], A[n-1])
+            end if
+        end for
+        generate(n - 1, A)
+    end if
 
 
 /* doesn't help (purely coincidental) */ 

512e8efb4aeab9fe29d1da56bfffb9a4e2829f33
M. Manousos <mmanousos@gmail.com>
2018-01-03 16:49:25 -0800

    adds fcc solution

--- makeaperson.js
+++ makeaperson.js
@@ -39,7 +39,39 @@ var Person = function(firstAndLast) {
 
 var bob = new Person('Bob Ross');
 
+/* fcc solution */
+// a little more streamlined (fewer variables created)
 
+var Person = function(firstAndLast) {
+  var fullName = firstAndLast;
+
+  this.getFirstName = function() {
+    return fullName.split(" ")[0];
+  };
+
+  this.getLastName = function() {
+    return fullName.split(" ")[1];
+  };
+
+  this.getFullName = function() {
+    return fullName;
+  };
+
+  this.setFirstName = function(name) {
+    fullName = name + " " + fullName.split(" ")[1];
+  };
+
+  this.setLastName = function(name) {
+    fullName = fullName.split(" ")[0] + " " + name;
+  };
+
+  this.setFullName = function(name) {
+    fullName = name;
+  };
+};
+
+var bob = new Person('Bob Ross');
+bob.getFullName();
 
 /* a solution that doesn't pass the tests */
 // doesn't return new full names after setting new names */ 

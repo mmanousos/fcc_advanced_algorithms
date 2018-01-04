9b3fdd2aa7d4e3db294f6434f8ee2b5c3877ee27
M. Manousos <mmanousos@gmail.com>
2018-01-04 10:42:13 -0800

    fcc intermediate solution

--- mapthedebris.js
+++ mapthedebris.js
@@ -1,4 +1,4 @@
-
+/* my solution */
 function orbitalPeriod(arr) {
   var GM = 398600.4418;
   var earthRadius = 6367.4447;
@@ -17,6 +17,29 @@ function orbitalPeriod(arr) {
   return arr;
 }
 
+orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]);
+
+
+/* fcc intermediate solution */ 
+
+function orbitalPeriod(arr) {
+  var GM = 398600.4418;
+  var earthRadius = 6367.4447;
+
+  //Looping through each key in arr object
+  for(var prop in arr) {
+    //Rounding off the orbital period value
+    var orbitalPer = Math.round(2 * Math.PI * Math.sqrt(Math.pow(arr[prop].avgAlt + earthRadius, 3) / GM));
+    //deleting the avgAlt property
+    delete arr[prop].avgAlt;
+    //adding orbitalPeriod property
+    arr[prop].orbitalPeriod = orbitalPer;
+  }
+
+  return arr;
+}
+
+// test here
 orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]);
 
 

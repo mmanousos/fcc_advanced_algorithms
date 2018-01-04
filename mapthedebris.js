f0d684e5e959b46c1647d9e6575292c3d71ee7fc
M. Manousos <mmanousos@gmail.com>
2018-01-04 10:38:41 -0800

    solution

--- mapthedebris.js
+++ mapthedebris.js
@@ -1,4 +1,25 @@
 
+function orbitalPeriod(arr) {
+  var GM = 398600.4418;
+  var earthRadius = 6367.4447;
+  
+    for (var i = 0; i < arr.length; i++) {
+    var object = arr[i];
+    var alt = (object.avgAlt);
+    var axis = alt + earthRadius; 
+    var a = Math.pow(axis, 3); 
+    var root = Math.sqrt(a/GM); 
+    var period = Math.round(2 * Math.PI * root);
+    object.orbitalPeriod = period;
+    delete object.avgAlt;
+  }
+  
+  return arr;
+}
+
+orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]);
+
+
 /*-- PSEUDOCODE --*/
 
 
